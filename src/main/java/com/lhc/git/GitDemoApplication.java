9e58fe01bc8231f1f825e1b3c309378a891ddd25
lhc0821 <1013217387@qq.com>
2019-07-24 09:31:28 +0800

    test1 分支 C4提交

--- src/main/java/com/lhc/git/GitDemoApplication.java
+++ src/main/java/com/lhc/git/GitDemoApplication.java
@@ -10,6 +10,7 @@ public class GitDemoApplication {
         System.out.println("commit change c1");
         System.out.println("commit change c2");
         System.out.println("test1 commit change c3");
+        System.out.println("test1 commit change c4");
         SpringApplication.run(GitDemoApplication.class, args);
     }
 
